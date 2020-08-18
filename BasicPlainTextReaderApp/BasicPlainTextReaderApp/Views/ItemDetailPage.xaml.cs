af8c8e2269ab6983897205a9089b7a0f9127b60f
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-08-18 18:31:37 -0500

    Add project files.

---
+++ BasicPlainTextReaderApp/BasicPlainTextReaderApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using BasicPlainTextReaderApp.ViewModels;
+
+namespace BasicPlainTextReaderApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
