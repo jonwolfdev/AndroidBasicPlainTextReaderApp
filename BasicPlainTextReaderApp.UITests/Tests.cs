f16b7a8445d98ed600c5f796b0b4fd7ce8183eb2
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2020-08-22 13:08:44 -0500

    Added UI test for welcome page and file/search

--- BasicPlainTextReaderApp.UITests/Tests.cs
+++ BasicPlainTextReaderApp.UITests/Tests.cs
@@ -12,7 +12,7 @@ namespace BasicPlainTextReaderApp.UITests
     public class Tests
     {
         IApp app;
-        Platform platform;
+        readonly Platform platform;
 
         public Tests(Platform platform)
         {
@@ -29,12 +29,82 @@ namespace BasicPlainTextReaderApp.UITests
         public void WelcomeTextIsDisplayed()
         {
             AppResult[] results = app.WaitForElement(c => c.Marked("Basic Text Reader App"));
-            //app.Screenshot("Welcome screen.");
-
-            Assert.IsTrue(results.Any());
             
+            Assert.IsTrue(results.Any());
+            var elem = app.Query("Made by @jonwolfdev").FirstOrDefault();
+            Assert.IsNotNull(elem);
+        }
+
+        [Test]
+        public void OpenFileIsDisplayedAndSearchWorks()
+        {
+            string fileContents = "Test file\nTest file line 2\n";
+            string filePath = "/data/user/0/dev.jonwolf.basicplaintextreaderapp/files/test.txt";
+            string searchPopupText = "Enter the text to search for";
+            string firstSearch = "file";
+            string secondSearch = "line";
+            string buttonPopupInfoClose = "button2";
+            string buttonPopupSearchSearch = "button1";
             app.Invoke("OpenTextFile");
-            AppResult[] results2 = app.WaitForElement(c => c.Id("InfoItemAi"));
+
+            AppResult[] results2 = app.WaitForElement(c => c.Marked(filePath));
+            Assert.AreEqual(1, results2.Count());
+
+
+            var label = app.Query(fileContents).FirstOrDefault();
+            Assert.IsNotNull(label);
+            Assert.AreEqual(fileContents, label.Text);
+            app.Tap("Info");
+
+            // Popup message
+            AppResult[] resultsMsg = app.WaitForElement(c => c.Marked("message"));
+            Assert.AreEqual(1, resultsMsg.Length);
+
+            Assert.IsTrue(resultsMsg[0].Text.Contains("Type: text/plain"));
+            Assert.IsTrue(resultsMsg[0].Text.Contains("DataPath: " + filePath));
+
+            // Close button
+            app.Tap(buttonPopupInfoClose);
+            app.WaitForNoElement("message");
+            {
+                app.Tap("Search");
+                // Popup search
+                var searchPopup = app.WaitForElement("message");
+                Assert.IsTrue(searchPopup.Any());
+
+                Assert.AreEqual(searchPopupText, searchPopup[0].Text);
+                app.EnterText(firstSearch);
+                // Search button
+                app.Tap(buttonPopupSearchSearch);
+
+                app.WaitForElement("Found 2 matches");
+
+                var elemMatches = app.WaitForElement(fileContents);
+                Assert.AreEqual(2, elemMatches.Length);
+            }
+
+            // Search again but within the same search page
+            {
+                app.Tap("Search");
+                // Popup search
+                var searchPopup = app.WaitForElement("message");
+                Assert.IsTrue(searchPopup.Any());
+
+                Assert.AreEqual(searchPopupText, searchPopup[0].Text);
+                app.EnterText(secondSearch);
+                // Search button
+                app.Tap(buttonPopupSearchSearch);
+
+                app.WaitForElement("Found 1 matches");
+
+                var elemMatches = app.WaitForElement(fileContents);
+                Assert.AreEqual(1, elemMatches.Length);
+            }
+
+            app.Back();
+
+            AppResult[] resultsBack = app.WaitForElement(c => c.Marked(filePath));
+            Assert.AreEqual(1, resultsBack.Count());
         }
 
         
